ab88799d4e51b241c246bde49cc1de13aaffe66b
flash1999 <noursoury99@gmail.com>
2025-07-23 12:57:52 +0300

    Revert "Added a Json parser"
    This reverts commit 7b0d4e53b9cc24178186575f6e8520d1966a9175.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import logger from "./util/logger";
 
 import { readCSVFile } from "./util/parser";
-import { readJSONFile } from "./util/parsers/jsonParser";
 
 
 async function main() {
-    /*
     const data = await readCSVFile("src/data/cake-orders.csv", true);
     // for each data row, log the row
     data.forEach((row) => logger.info(row));
-    */
-   try {
-       const data = await readJSONFile("src/data/book.json");
-       console.log('Read data:', data);
-   } catch (error) {
-       logger.error("Error reading JSON file:", error);
-   }
 }
 
 main();

c39d625d44b700faa2854266cfe84aeb11e07528
flash1999 <noursoury99@gmail.com>
2025-07-24 14:51:06 +0300

    Implemented builder pattern for cake model

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import logger from "./util/logger";
+import { CakeBuilder } from "./model/builders/cake.builder";
+import { Cake } from "model/Cake.model";
 
-import { readCSVFile } from "./util/parser";
-
-
+logger.info('App started');
 async function main() {
-    const data = await readCSVFile("src/data/cake-orders.csv", true);
-    // for each data row, log the row
-    data.forEach((row) => logger.info(row));
+    const cakeBuilder = new CakeBuilder();
+    const cake = cakeBuilder
+        .setType("type")
+        .setFlavor("flavor")
+        .setFilling("filling")
+        .setSize(10)
+        .setLayers(2)
+        .setFrostingType("frostingType")
+        .setFrostingFlavor("frostingFlavor")
+        .setDecorationType("decorationType")
+        .setDecorationColor("decorationColor")
+        .setCustomMessage("customMessage")
+        .setShape("shape")
+        .setAllergies("allergies")
+        .setSpecialIngredients("specialIngredients")
+        .setPackagingType("packagingType")
+        .build();
+  
+  console.log(cake);
+  
 }
 
 main();
